6562f1eea588febebfefcefcaca28de0e7416a56
Jessica Dong <jessicadong101@berkeley.edu>
2023-05-26 00:02:26 -0700

    add some css changes, clean up code

--- index.js
+++ index.js
@@ -20,67 +20,28 @@ function getInput() {
     month = month.value; 
     year = year.value;
 
-    let months_31 = [1, 3, 5, 7, 8, 10, 12];
-
-    var error_vals = 0;
     /** Check for empty or invalid values */
-
-    // day
-    if (day == "") {
-        document.getElementById('error-day').innerHTML = "This field is required.";
-        error_vals++;
-    } else if (day < '1' || day > '31') {
-        document.getElementById('error-day').innerHTML = "Must be a valid day.";
-        error_vals++;
-    } else if ((day > '28' && month_t == 2 && year_t % 4 != 0) 
-    || (day > '29' && month_t == 2 && year_t % 4 == 0)) {
-        document.getElementById('error-day').innerHTML = "Must be a valid day.";
-        error_vals++;
-    } else if (day > '30' && !months_31.includes(month_t)) {
-        document.getElementById('error-day').innerHTML = "Must be a valid day.";
-        error_vals++;
-    } else {
-        document.getElementById('error-day').innerHTML = "";
-    }
-
-    // month
-    if (month == "") {
-        document.getElementById('error-month').innerHTML = "This field is required.";
-        error_vals++;
-    } else if (month < 1 || month > 12) {
-        document.getElementById('error-month').innerHTML = "Must be a valid month.";
-        error_vals++;
-    } else {
-        document.getElementById('error-month').innerHTML = "";
-    }
-
-    // year
-    if (year == "") {
-        document.getElementById('error-year').innerHTML = "This field is required.";
-        error_vals++;
-    } else if (year > year_t) {
-        document.getElementById('error-year').innerHTML = "Must be in the past.";
-        error_vals++;
-    } else if (year == year_t && month > month_t) {
-        document.getElementById('error-year').innerHTML = "Must be in the past.";
-        error_vals++;
-    } else if (year == year_t && month == month_t && day > day_t) {
-        document.getElementById('error-year').innerHTML = "Must be in the past.";
-        error_vals++;
-    } else {
-        document.getElementById('error-year').innerHTML = "";
-    }
-
-   
+    var error_vals = isValidDay(day, month, year);
+    error_vals += isValidMonth(month);
+    error_vals += isValidYear(month, year, month_t, year_t);
 
     if (error_vals > 0) {
         // change all styles to red
-        console.log("error vals > 0");
-        document.getElementById('top').style.color = "red";
+        document.getElementById('top').style.color = "var(--light_red)";
+        var elements = document.getElementsByTagName('input');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].border = "2px solid var(--light_red)";
+        }
+        age_day = "--";
+        age_month = "--";
+        age_year = "--";
     } else {
-        document.getElementById('top').style.color = "black";
+        document.getElementById('top').style.color = "var(--smokey_grey)";
+        var elements = document.getElementsByTagName('input');
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.border = "2px solid var(--light_grey)";
+        }
         
-
         if (day_t < day) {
             const temp = new Date(year_t, month_t, 0);
             day_t += temp.getDate();
@@ -94,24 +55,79 @@ function getInput() {
         age_day = day_t - day;
         age_month = month_t - month;
         age_year = year_t - year;
-
-        getAge();
     }
+
+    getAge();
+}
+
+function isValidDay(day, month, year) {
+    let months_31 = [1, 3, 5, 7, 8, 10, 12];
+
+    if (day == "") {
+        document.getElementById('error-day').innerHTML = "This field is required.";
+        return 1;
+    } else if (day < 1 || day > 31) {
+        document.getElementById('error-day').innerHTML = "Must be a valid day.";
+        return 1;
+    } else if (month == 2) {
+        if (day > 29) {
+            document.getElementById('error-day').innerHTML = "Must be a valid day.";
+            return 1;
+        } else if (year % 4 != 0 && day == 29) {
+            document.getElementById('error-day').innerHTML = "Must be a valid day.";
+            return 1;
+        } 
+    } else if (day > 30 && !months_31.includes(month)) {
+        document.getElementById('error-day').innerHTML = "Must be a valid day.";
+       return 1;
+    } 
+    document.getElementById('error-day').innerHTML = "";
+    return 0;
+}
+
+function isValidMonth(month) {
+    if (month == "") {
+        document.getElementById('error-month').innerHTML = "This field is required.";
+        return 1;
+    } else if (month < 1 || month > 12) {
+        document.getElementById('error-month').innerHTML = "Must be a valid month.";
+        return 1;
+    } 
+    document.getElementById('error-month').innerHTML = "";
+    return 0;
+}
+
+function isValidYear(month, year, month_t, year_t) {
+    if (year == "") {
+        document.getElementById('error-year').innerHTML = "This field is required.";
+        return 1;
+    } else if (year > year_t) {
+        document.getElementById('error-year').innerHTML = "Must be in the past.";
+        return 1;
+    } else if (year == year_t && month > month_t) {
+        document.getElementById('error-year').innerHTML = "Must be in the past.";
+        return 1;
+    } else if (year == year_t && month == month_t && day > day_t) {
+        document.getElementById('error-year').innerHTML = "Must be in the past.";
+        return 1;
+    } 
+    document.getElementById('error-year').innerHTML = "";
+    return 0;
 }
 
 function getAge() {
     let year = document.getElementById('age-year');
     if (year) {
-        year.innerHTML = age_year + " years";
+        year.innerHTML = age_year;
     }
 
     let month = document.getElementById('age-month');
     if (month) {
-        month.innerHTML = age_month + " months";
+        month.innerHTML = age_month;
     }
 
     let day = document.getElementById('age-day');
     if (day) {
-        day.innerHTML = age_day + " days";
+        day.innerHTML = age_day;
     }
 }
